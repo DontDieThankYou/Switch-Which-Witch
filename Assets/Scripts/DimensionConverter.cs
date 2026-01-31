ab5b8ffca8ff95591433826bb1a830e666fc7e39
tab <58062648+tabithacatitha@users.noreply.github.com>
2026-01-31 00:04:15 -0500

    nice dimension converter

---
+++ Assets/Scripts/DimensionConverter.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public static class DimensionConverter
+{
+    public static Vector3 XZtoXYZ(Vector2 input, float y = 0.0f)
+    {
+        return new Vector3(input.x, y, input.y);
+    }
+
+    public static Vector3 XYZtoXZ(Vector2 input)
+    {
+        return new Vector2(input.X, input.Z);
+    }
+}
