cbab77dacb5ff9b83f40f276b90ec442f58ab40b
TavioFicaccio <43261359+Tavio1@users.noreply.github.com>
2026-01-31 15:48:39 -0500

    Beginning of the town

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
 
+[ExecuteAlways]
 public class Billboard : MonoBehaviour
 {
     // Use LateUpdate to ensure all camera movements are finalized this frame
     void LateUpdate()
     {
+        rotateToCam();
+    }
+
+
+
+    void rotateToCam() {
+        
         // Make the sprite's forward direction match the camera's forward direction
         transform.forward = Camera.main.transform.forward;
     }
